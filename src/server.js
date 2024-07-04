dfd9a78700bac5eb5c235a42607bf96726573e3b
Hoeyun <yang3522@gmail.com>
2024-07-04 14:56:17 +0900

    Severs Recap

--- src/server.js
+++ src/server.js
@@ -4,32 +4,9 @@ const PORT = 4000;
 
 const app = express();
 
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
+const handleHome = () => console.log("home");
 
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("<h1>Not Allowed</h1>");
-  }
-  console.log("Allowed, you may continue");
-  next();
-};
-
-const handleHome = (req, res) => {
-  return res.send("I love middlewares!");
-};
-
-const handleProtected = (req, res) => {
-  return res.send("Welcome");
-};
-
-app.use(logger);
-app.use(privateMiddleware);
 app.get("/", handleHome);
-app.get("/protected", handleProtected);
 
 const handleListening = () => console.log(`✅ Server listening on port http://localhost:${PORT}`);
 
