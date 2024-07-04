92c343f76acd9e61f743b8245d4880b06ae3c7fa
Hoeyun <yang3522@gmail.com>
2024-07-04 15:25:28 +0900

    Middleware Recap

--- src/server.js
+++ src/server.js
@@ -4,9 +4,27 @@ const PORT = 4000;
 
 const app = express();
 
-const home = (req, res) => res.send("<h1>Hello!</h1>");
+const routerLogger = (req, res, next) => {
+  console.log("PATH : ", req.path);
+  next();
+};
 
+const methodLogger = (req, res, next) => {
+  console.log("METHOD : ", req.method);
+  next();
+};
+
+const home = (req, res) => {
+  console.log("I will respond.");
+  return res.send("<h1>Hello!</h1>");
+};
+
+const login = (req, res) => {
+  return res.send("login");
+};
+app.use(methodLogger, routerLogger);
 app.get("/", home);
+app.get("/login", login);
 
 const handleListening = () => console.log(`✅ Server listening on port http://localhost:${PORT}`);
 
