bd575146940c522fb11a587c4728c9d7dfc4c34b
Hoeyun <yang3522@gmail.com>
2024-07-04 15:32:49 +0900

    External Middlewares - add morgan

--- src/server.js
+++ src/server.js
@@ -1,18 +1,10 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
+const logger = morgan("dev");
 
 const app = express();
-
-const routerLogger = (req, res, next) => {
-  console.log("PATH : ", req.path);
-  next();
-};
-
-const methodLogger = (req, res, next) => {
-  console.log("METHOD : ", req.method);
-  next();
-};
 
 const home = (req, res) => {
   console.log("I will respond.");
@@ -22,7 +14,8 @@ const home = (req, res) => {
 const login = (req, res) => {
   return res.send("login");
 };
-app.use(methodLogger, routerLogger);
+
+app.use(logger);
 app.get("/", home);
 app.get("/login", login);
 
