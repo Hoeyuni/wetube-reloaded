268bdd1281010da9d08b1a1b172e752cd62bb496
Hoeyun <yang3522@gmail.com>
2024-07-23 13:59:50 +0900

    Form Validation($or, 패스워드 확인)

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,7 +3,21 @@ import User from "../models/User";
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
   console.log(req.body);
-  const { name, username, email, password, location } = req.body;
+  const { name, username, email, password, password2, location } = req.body;
+  const pageTitle = "Join";
+
+  if (password !== password2) {
+    // 패스워드 확인
+    return res.render("join", { pageTitle, errorMessage: "Password confirmation does not match" });
+  }
+
+  const exists = await User.exists({ $or: [{ username: req.body.username }, { email }] });
+  if (exists) {
+    return res.render("join", { pageTitle, errorMessage: "This username/email is already taken" });
+  }
+
+  // $or:[{a},{b}] : 배열 중 어떤 조건이 하나라도 true면 해당 데이터를 찾아온다,
+
   await User.create({
     name,
     username,
